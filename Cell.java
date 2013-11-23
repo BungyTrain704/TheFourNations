8a2f4cd275254152889f2e1bf2104d1d8ebcfaba
mleones <leones@email.arizona.edu>
2013-11-23 14:38:59 -0700

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,70 @@
+import java.util.Random;
+
+public class Cell {
+	
+	private boolean hasUnit;
+	private boolean isVisible;
+	private boolean hasResource;
+	private Terrain terrain;
+	private Resource resource;
+	private int average;
+	private Random rand = new Random();
+	
+	public Cell() {
+		terrain= Terrain.plains;
+		hasResource = false;
+		hasUnit = false;
+		isVisible = false;
+		average = rand.nextInt(300);
+	}
+
+	public void setTerrain(Terrain t) {
+		terrain = t;
+	}
+	
+	public String getTerrain(){
+		return terrain.getTerrain();
+	}
+	
+	public void setUnit(boolean unitHere) {
+		hasUnit = unitHere;
+	}
+	
+	public boolean hasUnit(){
+		return hasUnit;
+	}
+	
+	public void setVisible() {
+		isVisible = true;
+	}
+	
+	public boolean visibility() {
+		return isVisible;
+	}
+	
+	public void setAverage(int avg) {
+		average = avg;
+	}
+	
+	public int getAverage() {
+		return average;
+	}
+	
+	public void setResource(Resource res) {
+		resource = res;
+		hasResource = true;
+	}
+	
+	public String getResource() {
+		return resource.getResource();
+	}
+	
+	public boolean hasResource() {
+		return hasResource;
+	}
+	
+	public void removeResource() {
+		resource = null;
+		hasResource = false;
+	}
+}
