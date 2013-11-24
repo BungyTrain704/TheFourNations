d1f3983ece1c35687ae1cbff1e32edb663527f53
Chris <cachapline8@gmail.com>
2013-11-24 16:45:20 -0700

    Added comments to Terrain

--- Terrain.java
+++ Terrain.java
@@ -1,19 +1,41 @@
+/**
+ * The different types of terrain that can exist on the map,
+ * along with a symbol that can be used in their toString
+ * @author Emily Leones, Michelle Yung, Christopher Chapline, James Fagan
+ *
+ */
 public enum Terrain {
 
+	//Enumeration declarations
 	plains(true, " "), water(false, "~");
 
+	//Instance variables
 	private boolean accessible;
 	private String symbol;
 
-	Terrain(boolean access, String terrain) {
-		this.accessible = access;
-		this.symbol = terrain;
+	/**
+	 * Creates a terrain type that contains data about a specific location
+	 * @param accessible True if the the terrain is accessible to a unit (can a unit walk on it, etc).
+	 * @param symbol The symbol shown in the toString for this item of terrain
+	 */
+	Terrain(boolean accessible, String symbol) {
+		this.accessible = accessible;
+		this.symbol = symbol;
 	}
 
+	/**
+	 * Returns whether or not this terrain is accessible. This determines if
+	 * the terrain can be walked on, built on, etc.
+	 * @return True if the terrain can be walked on, built on, etc; false otherwise.
+	 */
 	public boolean isAccessible() {
 		return accessible;
 	}
-	
+
+	/**
+	 * The ASCII representation of this terrain type
+	 * @return The String that represents this terrain type
+	 */
 	public String getTerrain() {
 		return this.symbol;
 	}
