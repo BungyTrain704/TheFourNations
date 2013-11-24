339fb683a0b51270a39cf7a00cd6973d2523eaad
Chris <cachapline8@gmail.com>
2013-11-24 16:31:21 -0700

    Added comments to BuildStructureTask

--- BuildStructureTask.java
+++ BuildStructureTask.java
@@ -7,6 +7,14 @@ public class BuildStructureTask extends Task {
 
 	private AbstractStructure structureToBuild;
 	
+	/**
+	 * Creates a task that results in a structure being added to the map
+	 * @param work The amount of work required to complete this task (number of game ticks)
+	 * @param locWorker The location the worker working on this task will be
+	 * @param locTask The location that the task will be located at
+	 * @param map The map that this task takes place on
+	 * @param structure The structure being built during this task
+	 */
 	public BuildStructureTask(int work, int locWorker, int locTask, Map map, AbstractStructure structure ) {
 		super(work, locWorker, locTask, map);
 		this.structureToBuild = structure;
