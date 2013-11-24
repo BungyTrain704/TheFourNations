44756dadcb6637d2ba448cf493743dbc9c628e42
Chris <cachapline8@gmail.com>
2013-11-24 16:46:04 -0700

    Added comments to Resource

--- Resource.java
+++ Resource.java
@@ -1,14 +1,19 @@
-
+/**
+ * A resource is stored in a cell and can be collected and used
+ * in tasks undertaken by the units
+ * @author Emily Leones, Michelle Yung, Christopher Chapline, James Fagan
+ *
+ */
 public enum Resource {
-	
+
 	tree("#"), stone("^");
-	
+
 	private String symbol;
-	
+
 	Resource(String res) {
 		this.symbol = res;
 	}
-	
+
 	public String getResource() {
 		return this.symbol;
 	}
