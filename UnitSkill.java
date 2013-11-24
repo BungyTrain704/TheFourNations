2638d3ff19588e4fee3fec6b03e6890d22385773
Chris <cachapline8@gmail.com>
2013-11-24 16:43:08 -0700

    Added comments to UnitSkill

--- UnitSkill.java
+++ UnitSkill.java
@@ -1,4 +1,8 @@
-
+/**
+ * These are the possible skills that could affect a unit's proficiency in completing a task.
+ * @author Christopher Chapline, James Fagan, Emily Leones, Michelle Yung
+ *
+ */
 public enum UnitSkill {
 
 	HARVESTING, STONEBREAKING, CRAFTING, COOKING;
