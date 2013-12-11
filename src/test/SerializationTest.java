5fe8584b693f1e01c054d6f8e1b06fc8292065dc
Chris <cachapline8@gmail.com>
2013-12-10 18:34:15 -0700

    Added SaveLoadManager and more definitive serialization tests. The 2nd round of tests actually test file saving and loading as well as actual state persistance throughout the save/load cycle. These tests will save 2 files in the following location: 		%user.home%\The Four Nations\saves The two files saved represent 2 independent and separate game states.
    The SaveLoadManager includes 3 static methods that do the following:
    	Save a game's persistant state to a file
    	Load a game's persistant state from a file
    	List the save files currently in the save directory

--- src/test/SerializationTest.java
+++ src/test/SerializationTest.java
@@ -3,8 +3,12 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
 import model.Civilization;
 import model.CivilizationState;
+import model.SaveLoadManager;
 import model.map.Map;
 import model.map.Resource;
 import model.map.ResourceType;
@@ -45,5 +49,62 @@ public class SerializationTest {
 		assertFalse( civ.getStructures().contains(b) );
 		assertTrue( civ.getTaskQueue().isEmpty() );
 		
+	}
+	
+	@Test public void saveSomeStates() {
+		//Get civilization and save it
+		Civilization civ = Civilization.getInstance();
+		CivilizationState cs1 = new CivilizationState(civ);
+		SaveLoadManager.saveGame( "cs1", cs1 );
+		
+		//Change some stuff
+		civ.setResourceAmount( ResourceType.wood, 500 );
+		civ.setResourceAmount( ResourceType.stone, 300 );
+		
+		//Save another persistant state
+		CivilizationState cs2 = new CivilizationState(civ);
+		SaveLoadManager.saveGame( "cs2", cs2 );
+		
+		//Get the saves located in the file tree
+		System.out.print( "Existing saves: " );
+		ArrayList<String> firstTwoSaves = SaveLoadManager.getSavedGames();
+		System.out.println( firstTwoSaves );
+		assertEquals( 2, firstTwoSaves.size() );
+		System.out.println();
+
+		
+		//Load and test the first state
+		System.out.println( "CS1" );
+		System.out.println("------------------------------");
+		CivilizationState cs1Loaded = SaveLoadManager.loadGame("cs1");
+		System.out.println( "Structures:\t" + cs1Loaded.getStructures().size() );
+		System.out.println( "Units to Kill:\t" + cs1Loaded.getUnitsToKill().size() );
+		System.out.println( "Resource amounts:" );
+		for( ResourceType rt : ResourceType.values() ) {
+			System.out.println("\t" + rt + ":\t" + cs1.getGlobalResourcePool().get(rt) );
+		}
+		System.out.println( "Task Queue:\t" + cs1Loaded.getTaskQueue().size() );
+		assertEquals( 0, cs1Loaded.getStructures().size() );
+		assertEquals( 0, cs1Loaded.getUnitsToKill().size() );
+		for( ResourceType rt : ResourceType.values() ) assertEquals( 0, cs1.getGlobalResourcePool().get(rt).intValue());
+		assertEquals( 0, cs1Loaded.getTaskQueue().size() );
+		System.out.println();
+		
+		//Load and test the second state
+		System.out.println( "CS2" );
+		System.out.println("------------------------------");
+		CivilizationState cs2Loaded = SaveLoadManager.loadGame("cs2");
+		System.out.println( "Structures:\t" + cs2Loaded.getStructures().size() );
+		System.out.println( "Units to Kill:\t" + cs2Loaded.getUnitsToKill().size() );
+		System.out.println( "Resource amounts:\t" );
+		for( ResourceType rt : ResourceType.values() ) {
+			System.out.println("\t" + rt + ":\t" + cs2.getGlobalResourcePool().get(rt) );
+		}
+		System.out.println( "Task Queue:\t" + cs2Loaded.getTaskQueue().size() );
+		assertEquals( 0, cs2Loaded.getStructures().size() );
+		for( ResourceType rt : ResourceType.values() ) assertEquals( 0, cs1.getGlobalResourcePool().get(rt).intValue());
+		assertEquals( 0, cs2Loaded.getUnitsToKill().size() );
+		assertEquals( 0, cs2Loaded.getTaskQueue().size() );
+		System.out.println();
 	}
 }
