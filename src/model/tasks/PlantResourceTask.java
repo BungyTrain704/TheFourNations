320c110628d4c6801c6b0379c9ec6cbe87eda7a2
Chris <cachapline8@gmail.com>
2013-12-14 14:22:27 -0700

    Plant resource task

---
+++ src/model/tasks/PlantResourceTask.java
@@ -0,0 +1,47 @@
+package model.tasks;
+
+import model.Civilization;
+import model.exceptions.DisallowedTaskException;
+import model.map.Map;
+import model.map.Resource;
+
+/**
+ * Plants a resource at a specified location
+ * @author Christopher Chapline, James Fagan, Emily Leones, Michelle Yung
+ *
+ */
+public class PlantResourceTask extends Task {
+
+	
+	private static final long serialVersionUID = 1L; //TODO: fix this serialVersionUID
+	private Resource resourceToPlant;
+	
+	/**
+	 * Creates a task that results in a resource being added to the map
+	 * @param work The amount of work required to complete this task (number of game ticks)
+	 * @param location the location where the resource will go
+	 * @param map The map that this task takes place on
+	 * @param resource The resource being added
+	 */
+	public PlantResourceTask(int work, int location, Map map, Resource resource) {
+		super(work, location, map);
+		this.resourceToPlant = resource;
+		
+		if(!map.getCell(location).isAccessible() ) {
+			String message = "The location is not cleared, so a resource cannot be planted there.";
+			throw new DisallowedTaskException( this, message );
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * <p>Adds the resource to the map</p>
+	 */
+	@Override public void performAction() {
+		Civilization civ = Civilization.getInstance();
+		map.getCell(super.locationOfTask).setResource(resourceToPlant);
+		map.makeInaccesible(super.locationOfTask);
+		this.isDone = true;
+		
+	}
+}
